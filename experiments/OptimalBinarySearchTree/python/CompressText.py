873c1bd8cd5c4069ae5cf3cf0e7ed1ce800a67cc
Leonardo Florez-Valencia <florez-l@javeriana.edu.co>
2024-09-30 16:59:22 -0500

    ...

--- experiments/OptimalBinarySearchTree/python/CompressText.py
+++ experiments/OptimalBinarySearchTree/python/CompressText.py
@@ -3,10 +3,10 @@
 ## =========================================================================
 
 import itertools, re, sys, unidecode
-import OptimalBinarySearchTree
+import HuffmanTree, OptimalBinarySearchTree
 
-if len( sys.argv ) == 1:
-  print( 'Usage: python ' + sys.argv[ 0 ] + ' input.txt' )
+if len( sys.argv ) < 3:
+  print( 'Usage: python ' + sys.argv[ 0 ] + ' input.txt [opt/huf]' )
   sys.exit( 1 )
 # end if
 
@@ -36,7 +36,11 @@ P = [ float( f ) / float( len( M ) ) for f in H.values( ) ]
 Q = [ float( 0 ) for p in range( len( P ) + 1 ) ]
 
 # Solve the problem
-oT = OptimalBinarySearchTree.build( T, P, Q )
-print( oT )
+if sys.argv[ 2 ] == 'opt':
+  oT = OptimalBinarySearchTree.build( T, P, Q )
+else:
+  oT = HuffmanTree.build( T, P )
+  print( oT.encode( ) )
+# end if
 
 ## eof - $RCSfile$
---
+++ experiments/OptimalBinarySearchTree/python/HuffmanTree.py
@@ -0,0 +1,86 @@
+## =========================================================================
+## @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
+## =========================================================================
+
+import heapq
+from BinaryTree import BinaryTree
+
+"""
+"""
+class HuffmanTree( BinaryTree ):
+
+  '''
+  '''
+  def __init__( self, f = 0, v = None ):
+    super( ).__init__( ( f, v ) )
+  # end def
+
+  '''
+  '''
+  def __lt__( self, other ):
+    return self.m_V[ 0 ] < other.m_V[ 0 ]
+  # end def
+
+  '''
+  '''
+  def encode( self ):
+    if not self.m_V[ 1 ] is None:
+      return { self.m_V[ 1 ]: '' }
+    else:
+      l = None
+      r = None
+      if not self.m_L is None and not self.m_R is None:
+        l = self.m_L.encode( )
+        r = self.m_R.encode( )
+      elif self.m_L is None and not self.m_R is None:
+        l = self.m_L.encode( )
+      elif not self.m_L is None and self.m_R is None:
+        r = self.m_R.encode( )
+      # end if
+
+      if not l is None:
+        for t in l:
+          l[ t ] = '0' + l[ t ]
+        # end for
+      # end if
+      if not r is None:
+        for t in r:
+          r[ t ] = '1' + r[ t ]
+        # end for
+      # end if
+
+      if not l is None and not r is None:
+        return { **l, **r }
+      elif l is None and not r is None:
+        return l
+      elif not l is None and r is None:
+        return r
+      else:
+        return None
+      # end if
+
+    # end if
+  # end def
+# end class
+
+'''
+'''
+def build( T, P ):
+  H = [ HuffmanTree( f, v ) for f, v in zip( P, T ) ]
+  heapq.heapify( H )
+
+  while len( H ) > 1:
+    r = heapq.heappop( H )
+    l = heapq.heappop( H )
+
+    n = HuffmanTree( l.m_V[ 0 ] + r.m_V[ 0 ] )
+    n.set_left( l )
+    n.set_right( r )
+
+    heapq.heappush( H, n )
+  # end while
+
+  return H[ 0 ]
+# end def
+
+## eof - $RCSfile$
