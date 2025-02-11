10225d7619b10482bdc7ba25aa7fb6dc5689453d
Leonardo Florez-Valencia <florez-l@javeriana.edu.co>
2025-02-11 08:31:02 -0500

    ...

--- experiments/Sorting.cxx
+++ experiments/Sorting.cxx
@@ -13,11 +13,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
-/* TODO
-   #include <iterator>
-   #include <random>
-   
-*/
+
 #include <PUJ/Sorting/Helpers.h>
 #include <PUJ/Sorting/Bubble.h>
 #include <PUJ/Sorting/Insertion.h>
@@ -120,32 +116,7 @@ int main( int argc, char** argv )
     std::cout << std::endl;
   } // end for
 
-  /* TODO
-     unsigned int N = 1000;
-     unsigned int S = 10;
-     if( argc > 1 ) N = std::atoi( argv[ 1 ] );
-     if( argc > 2 ) S = std::atoi( argv[ 2 ] );
-
-     // Prepare some data: use same seed to get always same numbers
-     std::vector< int > v( N );
-     std::mt19937 gen( 0 );
-     std::uniform_int_distribution< decltype( v )::value_type > d( -100, 100 );
-     for( unsigned int n = 0; n < N; ++n )
-     v[ n ] = d( gen );
-
-     // Execute experiments
-     for( unsigned int n = 0; n <= N; n += S )
-     {
-     decltype( v ) w( v.begin( ), v.begin( ) + n );
-     auto s = std::chrono::steady_clock::now( );
-     PUJ::Sorting::Insertion( w.begin( ), w.end( ) );
-     auto d = std::chrono::steady_clock::now( ) - s;
-     auto t = std::chrono::duration_cast< std::chrono::nanoseconds >( d );
-
-     std::cout << n << " " << t.count( ) << " " << std::endl;
-     } // end for
-  */
   return( EXIT_SUCCESS );
 }
 
-// eof - LoopInsertionSort.cxx
+// eof - Sorting.cxx
---
+++ experiments/Sorting.py
@@ -0,0 +1,72 @@
+## =========================================================================
+## @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
+## =========================================================================
+
+import sys, time
+sys.path.append( '../lib/python3' )
+import PUJ.Sorting
+
+## -------------------------------------------------------------------------
+def test_sort( S, f ):
+  L = S.copy( )
+  s = time.time_ns( )
+  f( L )
+  ns = time.time_ns( ) - s
+  if not PUJ.Sorting.Helpers.is_sorted( L ):
+    raise Exception( 'Sequence was not ordered!: **', f, '**' )
+  # end if
+  print( '{:.4e}'.format( ns * 1e-9 ), end = ' ' )
+# end def
+
+
+## --------------------------------- MAIN ----------------------------------
+if __name__ == '__main__':
+  if len( sys.argv ) < 4:
+    print(
+      'Usage: python ' + sys.argv[ 0 ] +
+      ' datafile size step' +
+      ' [permutation] [bubble] [insertion] [quick] [merge] [tim]'
+      )
+    sys.exit( 1 )
+  # end if
+  datafile = sys.argv[ 1 ]
+  size = int( sys.argv[ 2 ] )
+  step = int( sys.argv[ 3 ] )
+
+  # -- Read some data
+  datafile_ifs = open( datafile, 'rb' )
+  S = list( datafile_ifs.read( ) )[ : size ]
+  datafile_ifs.close( )
+
+  # -- Get algoritms
+  algorithms = {
+    'permutation' : PUJ.Sorting.Permutation,
+    'bubble'      : PUJ.Sorting.Bubble,
+    'insertion'   : PUJ.Sorting.Insertion,
+    'quick'       : PUJ.Sorting.Quick,
+    'merge'       : PUJ.Sorting.Merge,
+    'tim'         : PUJ.Sorting.Tim,
+    }
+  A = [ a.lower( ) for a in sys.argv[ 4 : ] ]
+
+  print( 'size ', end = '' )
+  for a in A:
+    if a in algorithms:
+      print( a, end = ' ' )
+    # end if
+  # end for
+  print( '' )
+
+  # Process data
+  for n in range( 0, size + 1, step ):
+    print( str( n ) + ' ', end = '' )
+    for a in A:
+      if a in algorithms:
+        test_sort( S[ 0: n ], algorithms[ a ] )
+      # end if
+    # end for
+    print( '' )
+  # end for
+# end if
+
+## eof - $RCSfile$
